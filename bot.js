8229e646c46189343fd9c5f02940c36cede4a9cd
vikornienko <vikornienko76@gmail.com>
2024-11-04 16:50:39 +0500

    Third commit. Request to the API and receive information.

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 import { Telegraf } from "telegraf";
 import { message } from "telegraf/filters";
 
-import "dotenv/config";
+import "dotenv/config.js";
 import axios from "axios";
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
@@ -15,7 +15,28 @@ bot.command('start', async (ctx) => {
 bot.command('getinfo', async (ctx) => {
     await ctx.reply(
         "Запрашиваю информацию с сайта!"
+    );
+    const ratesArray = await getRates();
+    const rates = ratesArray.filter((res, index) => index < 3).map(
+        item => {
+            const percent =
+              item.quote.USD.percent_change_1h > 0
+                ? `Рост на ${item.quote.USD.percent_change_1h.toFixed(2)}%`
+                : `Понижение на ${item.quote.USD.percent_change_1h.toFixed(2)}`;
+            return `${item.symbol} |\$${item.quote.USD.price.toFixed(2)} | ${percent}`
+        }
     )
+    ctx.reply(rates.join('\n'));
+    console.log(ratesArray);
 })
+
+const getRates = async () => {
+    const result = await axios({
+      method: "GET",
+      url: "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest",
+      headers: { "X-CMC_PRO_API_KEY": process.env.API_KEY },
+    });
+    return result.data.data
+}
 
 bot.launch();
