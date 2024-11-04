57f80a6a0eb544b5c1baf8026c6d7e86ece3c135
vikornienko <vikornienko76@gmail.com>
2024-11-04 15:14:08 +0500

    Second commit. Was install axios. Added bot command getinfo.

--- bot.js
+++ bot.js
@@ -2,11 +2,20 @@ import { Telegraf } from "telegraf";
 import { message } from "telegraf/filters";
 
 import "dotenv/config";
+import axios from "axios";
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 bot.command('start', async (ctx) => {
-    await ctx.telegram.sendMessage(ctx.message.chat.id, `Hello ${ctx.state.role}`)
+    await ctx.reply(
+        "Привет! Я бот, который по команде /getinfo покажет тебе курсы ТОП-10 криптовалют с сайта coinmarketcap"
+    )
 });
+
+bot.command('getinfo', async (ctx) => {
+    await ctx.reply(
+        "Запрашиваю информацию с сайта!"
+    )
+})
 
 bot.launch();
